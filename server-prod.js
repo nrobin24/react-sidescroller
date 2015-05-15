7513740055a489d2152de8d41c797c7c581d7ba7
Nick Robin <nickrobin@Nicks-MacBook-Pro.local>
2015-05-15 13:56:56 -0700

    prep for dokku deployment

---
+++ server-prod.js
@@ -0,0 +1,12 @@
+var PORT = process.ENV.PORT || 8080;
+var express = require("express");
+
+var app = express();
+app.use(app.router);
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function(req, res){
+  res.send("Hello, World!");
+});
+
+app.listen(PORT);

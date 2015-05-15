c231cee7f4ac0840b3ec82de8862fa32633a1268
Nick Robin <nickrobin@Nicks-MacBook-Pro.local>
2015-05-15 14:23:36 -0700

    use static route to index.html in prod server

--- server-prod.js
+++ server-prod.js
@@ -3,10 +3,6 @@ var express = require("express");
 
 var app = express();
 
-app.use(express.static(__dirname + "/public"));
-
-app.get("/", function(req, res){
-  res.send("Hello, World!");
-});
+app.use(express.static(__dirname + "/"));
 
 app.listen(PORT);
